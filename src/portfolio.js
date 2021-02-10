e89214ac5a9ad180f4ffd48a0cf78accff7d25f8
github username <github email address>
2021-02-10 12:24:55 -0600

    first

---
+++ src/portfolio.js
@@ -0,0 +1,408 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//Home Page
+const greeting = {
+  title: "Philip Roush.",
+  title2: "Philip Roush",
+  logo_name: "PDR",
+  nickname: "",
+  full_name: "Philip Roush",
+  subTitle:
+    "Full Stack Developer, Web Development Enthusiast. Always learning.",
+  resumeLink:
+    "https://media-exp1.licdn.com/dms/document/C4D2DAQFjsD5thMHomw/profile-treasury-document-pdf-analyzed/0/1612454756978?e=1613062800&v=beta&t=Hrte66AwL5u_QLW4LkRX0lyRnMw8M7PDcc5Q2TigTzU",
+  mail: "mailto:philipdroush@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/PhilipRoush?tab=repositories",
+  linkedin: "https://www.linkedin.com/in/philiproush/",
+  gmail: "philipdroush@gmail.com",
+  facebook: " https://www.facebook.com/philip.roush",
+  twitter: "https://twitter.com/HOOKem0",
+  instagram: "https://www.instagram.com/philiproush/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS and Rails Backend",
+        "⚡ Developing mobile applications using Flutter and React Native",
+        "⚡ Creating application backend in Rails, Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "Node/NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        {
+          skillName: "Go",
+          fontAwesomeClassname: "simple-icons:go",
+          style: {
+            color: "#00A7D0",
+          },
+        },
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Ruby on Rails",
+          fontAwesomeClassname: "simple-icons:ruby",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "C#",
+          fontAwesomeClassname: "simple-icons:csharp",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+    
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Texas State University",
+      subtitle: "B.S. in Geography",
+      logo_path: "Texas State.png",
+      alt_name: "TXST",
+      duration: "2011 - 2016",
+      descriptions: [
+        "⚡ The ability to visualize geographic data with their associated attribute information through GIS also enhances people’s understanding of locational problems.",
+        "⚡ Theories and real-world problem-solving skills in environmental, physical, and human geography, geographic information sciences, and geographic education.",
+        "⚡ I have implemented several projects based on what I've leart under my Information System courses. ",
+      ],
+      website_link: "http://mycatalog.txstate.edu/courses/geo/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Flatiron School",
+      subtitle: "Full stack web developer",
+      logo_path: "FLAT.png",
+      certificate_link:
+        "https://flatironschool.com/career-courses/coding-bootcamp/austin",
+      alt_name: "deeplearning.ai",
+      color_code: "#F6B808",
+    },
+    {
+      title: "Nucamp",
+      subtitle: "Intro to HTML, CSS, and Javascript",
+      logo_path: "Nucamp.png",
+      certificate_link:
+        "https://www.nucamp.co/bootcamp-overview/web-development-fundamentals",
+      alt_name: "deeplearning.ai",
+      color_code: "#E2405F",
+    },
+    {
+      title: "Minor",
+      subtitle: "Business Administration",
+      logo_path: "TXST.png",
+      certificate_link:
+        "http://mycatalog.txstate.edu/undergraduate/mccoy-business-administration/business-administration-minor/",
+      alt_name: "Texas State University",
+      color_code: "#8C151599",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Technical Work, Freelance and Volunteership",
+  description:
+    "Experienced in Ruby on Rails and React/JavaScript based programming with a background in Oil and Gas and Hospitality/ Customer Service.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Full Time Work",
+      experiences: [
+        {
+          title: "Valet Manager/Golf Services/Member Services",
+          company: "Discovery Land Company",
+          company_url: "https://discoverylandco.com/",
+          logo_path: "DLC.jpg",
+          duration: "Sept 2017 - Jan 2020",
+          location: "Las Vegas, NV-Amenia, NY-Coeur d'Alene,ID",
+          description:
+            "Worked for 3 different Discovery Land Company properties. Part of Summit Clubs-LV inaugural team - now premier property. Managed and Facilitated valet staff of 12-15 employees. Voted Best Manager for 2019.",
+          color: "#ee3c26",
+        },
+        {
+          title: "Geography Information Systems Manager & Account Payables Attendant",
+          company: "FieldPoint Petroleum Corporation",
+          company_url: " ",
+          logo_path: "FPP.jpeg",
+          duration: "April 2013 - January 2017",
+          location: "Austin, TX",
+          description:
+            "Managed and supervised 3 employees. Composed 100+ maps of companies current/potential oil wells. Processed average of 100 invoices per week using OGsys accounting software. Obtained and entered more than 100 vendor w-9. Responsible for process of annual filings of 1099s.",
+          color: "#0071C5",
+        },
+        {
+          title: "Freelance Web Developer",
+          company: "Latipac Proporty Management",
+          company_url: " ",
+          logo_path: "LPM.jpeg",
+          duration: "November 2020 - Current",
+          location: "",
+          description: "Adjusted design parameters to boost performance and incorporate new features. Boosted multiple companies SEO scores by at least 10%. Conducted testing and review of website design for responsiveness, clarity and effectiveness.",
+          color: "#56A4D3",
+        },
+      ],
+    },
+    {
+      title: "Volunteer",
+      experiences: [
+        {
+          title: "Real Estate Portfolio",
+          company: "Kay Roush Realty",
+          company_url: "",
+          logo_path: "google_logo.png",
+          duration: "Current",
+          location: "Remote",
+          description:
+            "Creating a Professional Porfolio for a local real estate agent. In process...",
+          color: "#4285F4",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "image0.jpeg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do, I write on Medium.",
+    link: "https://roushphilip.medium.com/message-to-myself-15-weeks-ago-cc0c9493f540",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+};
